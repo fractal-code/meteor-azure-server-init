be458d98dcfd48f3ace28239d4442842aa2a55ff
Rami Jarrar <rami@talos.email>
2019-03-30 07:16:41 +1100

    Bump version to 1.3.3

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 # -------------------------------
 # Meteor Azure
 # Server initialisation script
-# Version: 1.3.2
+# Version: 1.3.3
 # -------------------------------
 
 BUNDLE_DIR="D:/home/meteor-azure"
